2094cf1ffe0c42aeaf6ae685642ddac496596072
danilkushnerov <apple.pacel@gmail.com>
2024-03-05 11:15:35 +0300

    прописал тестов views

--- promo_site/company/views.py
+++ promo_site/company/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+from .models import Company
+
 # Create your views here.
+def company_render(request):
+    return render(request, "company.html", {'companys': Company.objects.all()})
--- promo_site/promo_site/settings.py
+++ promo_site/promo_site/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'company',
+    'house',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'promo_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
